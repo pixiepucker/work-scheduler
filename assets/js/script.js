3604fe443f5b2f703f47ce971b89ab5b30878a31
pixiepucker <autumnoffic@gmail.com>
2022-03-08 11:56:27 -0600

    reduced # of divs to increase readability, deleted vars for hour delegation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,5 @@ var timedEvents = {};
 var currentDate = moment().format("ddd, MMMM Do, YYYY, HH:mm:ss");
 $("#currentDay").append(currentDate);
 
+//create var to set time for each hour
 //apply color to timeblocks based on time
-var hour1 = moment().set('hour, 9');
-var hour2 = moment().set('hour', 10);
-var hour3 = moment().set('hour', 11);
-var hour4 = moment().set('hour', 12);
-var hour5 = moment().set('hour', 13);
-var hour6 = moment().set('hour', 14);
-var hour7 = moment().set('hour', 15);
-var hour8 = moment().set('hour', 16);
-var hour9 = moment().set('hour', 17);
