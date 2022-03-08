10807a309801e6a749bfcbf6f62aa28416fdc6bb
pixiepucker <autumnoffic@gmail.com>
2022-03-08 01:50:44 -0600

    initial files and setup of index.html

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// var timedEvents = {};
+
+// //display current date in header
+
+
+// var createEvent = function(eventText, eventHour, eventlist) {
+//     //create element that will make up event
+//     var eventLi = $("<li>").addClass("textarea");
+//     var eventP = $('<p>').addClass("m-1").text(eventText);
+
+//     //append p to li
+//     eventLi.append(eventP);
+
+// };
