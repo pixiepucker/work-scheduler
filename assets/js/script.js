e6aa7af74bac3f1c3a63f365fc09ab9a7c84c2f3
pixiepucker <autumnoffic@gmail.com>
2022-03-08 23:03:52 -0600

    changed ul to textarea, set var for hours to determine color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
-// var timedEvents = {};
+var timedEvents = {};
 
-// //display current date in header
+//display current date in header
+var currentDate = moment().format("ddd, MMMM Do, YYYY, HH:mm:ss");
+$("#currentDay").append(currentDate);
 
-
-// var createEvent = function(eventText, eventHour, eventlist) {
-//     //create element that will make up event
-//     var eventLi = $("<li>").addClass("textarea");
-//     var eventP = $('<p>').addClass("m-1").text(eventText);
-
-//     //append p to li
-//     eventLi.append(eventP);
-
-// };
+//apply color to timeblocks based on time
+var hour1 = moment().set('hour, 9');
+var hour2 = moment().set('hour', 10);
+var hour3 = moment().set('hour', 11);
+var hour4 = moment().set('hour', 12);
+var hour5 = moment().set('hour', 13);
+var hour6 = moment().set('hour', 14);
+var hour7 = moment().set('hour', 15);
+var hour8 = moment().set('hour', 16);
+var hour9 = moment().set('hour', 17);
