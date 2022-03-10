efb01cec1ba97863b4f792cce3d254481e63453d
pixiepucker <autumnoffic@gmail.com>
2022-03-10 13:23:50 -0600

    wrote function that runs on load to populate colors, fixed css to darker future color

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,6 @@
 var currentDate = moment().format("ddd, MMMM Do, YYYY, HH:mm:ss");
 $("#currentDay").append(currentDate);
 
-//get current time with hours
-var currentTime = moment().format('HH');
-
 //save to localStorage through .saveBtn setItem
 $('.saveBtn').on('click', function(event) {
     //prevent default browser action
@@ -22,107 +19,61 @@ function loadEvents () {
     //create var to hold parsed string attached to 0900 div
     var eventHour1 = JSON.parse(localStorage.getItem('0900'));
     //pass event... var into .val to load in according to time div ex. 0900 1000 etc.
-    $('#first').val(eventHour1);
+    $('#09').val(eventHour1);
 
     var eventHour2 = JSON.parse(localStorage.getItem('1000'));
-    $('#second').val(eventHour2);
+    $('#10').val(eventHour2);
 
     var eventHour3 = JSON.parse(localStorage.getItem('1100'));
-    $('#third').val(eventHour3);
+    $('#11').val(eventHour3);
 
     var eventHour4 = JSON.parse(localStorage.getItem('1200'));
-    $('#fourth').val(eventHour4);
+    $('#12').val(eventHour4);
 
     var eventHour5 = JSON.parse(localStorage.getItem('1300'));
-    $('#fifth').val(eventHour5);
+    $('#13').val(eventHour5);
 
     var eventHour6 = JSON.parse(localStorage.getItem('1400'));
-    $('#sixth').val(eventHour6);
+    $('#14').val(eventHour6);
 
     var eventHour7 = JSON.parse(localStorage.getItem('1500'));
-    $('#seventh').val(eventHour7);
+    $('#15').val(eventHour7);
 
     var eventHour8 = JSON.parse(localStorage.getItem('1600'));
-    $('#eighth').val(eventHour8);
+    $('#16').val(eventHour8);
 
     var eventHour9 = JSON.parse(localStorage.getItem('1700'));
-    $('#ninth').val(eventHour9);
+    $('#17').val(eventHour9);
 
 };
 //call function to load in events
 loadEvents();
 
+//create loop to go over time blocks and compare to time
+//use jquery .each inside of .ready function to loop/run once page is loaded
+$(document).ready (function() {
+    //get current time with hours
+    var currentTime = moment().hours();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//apply color to timeblocks based on time
-// var timeBlocks = function () {
-//     //create var to hold current time
-//     var time = moment();
-    
-//     //set data for each div to equal time & hour
-//     var dataArr = [
-//         $('#hour1').data('time',9),
-//         $('#hour2').data('time',10),
-//         $('#hour3').data('time',11),
-//         $('#hour4').data('time',12),
-//         $('#hour5').data('time',13),
-//         $('#hour6').data('time',14),
-//         $('#hour7').data('time',15),
-//         $('#hour8').data('time',16),
-//         $('#hour9').data('time',17),
-//     ]  
-//     console.log(dataArr);
-
-//     for (var i = 0; i < dataArr.length; i++) {
-//         if (time < $('#hour1').data-time) {
-//             $('#hour1').addClass = ('future');
-//         } else if (time > $('#hour1').data-time) {
-//             $('#hour1').addClass = ('past');
-//         } else {
-//             $('#hour1').addClass = ('present');
-//         }
-//     }
-// };
-
-// timeBlocks();
+    $('.task').each(function() {
+        //var that holds parsed integer/(textarea)'s id (changed to numbers from strings)
+        var eventTime = parseInt($(this).attr('id'));
+        //for my sanity, check if function works
+        console.log('this function works!')
+        //use if statement to compare var eventTime with currentTime
+        //remove classes if conditions are/aren't met
+        if(currentTime > eventTime) {
+            //class past active
+            $(this).addClass('past');
+        } else if(currentTime === eventTime) {
+            //class present active
+            $(this).addClass('present');
+            $(this).removeClass('past');
+        } else if(currentTime < eventTime) {
+            //class future active
+            $(this).addClass('future');
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+        }
+    });
+});
